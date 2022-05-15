a63f7d328336ab2ac6bd375bf7f11a420e8724f6
Igor Gorin <42892166+tarihay@users.noreply.github.com>
2022-05-15 21:29:51 +0700

    Delete Lab2 directory

--- Lab2/src/main/java/ru/nsu/gorin/lab2/calculator/exceptions/ArgsAmountException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.gorin.lab2.calculator.exceptions;
-
-public class ArgsAmountException extends Exception {
-    public ArgsAmountException(String message) {
-        super(message);
-    }
-}
--- Lab2/src/main/java/ru/nsu/gorin/lab2/calculator/exceptions/CommandArgsAmountException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.gorin.lab2.calculator.exceptions;
-
-public class CommandArgsAmountException extends Exception {
-    public CommandArgsAmountException(String message) {
-        super(message);
-    }
-}
