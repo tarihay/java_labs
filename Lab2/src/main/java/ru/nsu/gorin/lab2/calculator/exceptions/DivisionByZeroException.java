cf96409df001e9cdbae7fc9b682b5a406a9e4f36
Igor Gorin <42892166+tarihay@users.noreply.github.com>
2022-05-15 21:25:58 +0700

    Delete lab2 directory

--- Lab2/src/main/java/ru/nsu/gorin/lab2/calculator/exceptions/DivisionByZeroException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.gorin.lab2.calculator.exceptions;
-
-public class DivisionByZeroException extends Exception {
-    public DivisionByZeroException(String message) {
-        super(message);
-    }
-}
